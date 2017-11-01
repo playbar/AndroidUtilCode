c5bc2bc87b7fd372c8cf1fea122eb43464edbd8e
playbar <hgl868@126.com>
2017-11-01 10:57:29 +0800

    modify code

--- app/src/main/java/com/blankj/androidutilcode/Config.java
+++ app/src/main/java/com/blankj/androidutilcode/Config.java
@@ -16,7 +16,7 @@ public class Config {
     public static final String LINE_SEP = System.getProperty("line.separator");
     public static final String PKG      = "com.blankj.androidutilcode";
     public static final String TEST_PKG = "com.blankj.testinstall";
-    public static final String GITHUB   = "https://github.com/playbar/AndroidUtils";
+    public static final String GITHUB   = "https://github.com/Blankj/AndroidUtilCode";
     public static final String BLOG     = "http://www.jianshu.com/u/46702d5c6978";
     public static final String CACHE_PATH;
     public static final String TEST_APK_PATH;
